c3f127d037a0a2f85f5836a68d874969db251afa
morispolanco <morispolanco@gmail.com>
2022-10-14 05:52:31 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,14 @@
 import streamlit as st
 from streamlit_chat import message
 import requests
-import os
-from unittest.util import _MAX_LENGTH
-import openai
-openai.api_key = os.getenv('OPENAI_API_KEY')
-
 
 st.set_page_config(
     page_title="Streamlit Chat - Demo",
     page_icon=":robot:"
 )
 
-API_URL = "https://api.openai.com/v1/completions"
-headers = {"Authorization": st.secrets['OPENAI_API_KEY']}
+API_URL = "https://api-inference.huggingface.co/models/facebook/blenderbot-400M-distill"
+headers = {"Authorization": st.secrets['api_key']}
 
 st.header("Streamlit Chat - Demo")
 st.markdown("[Github](https://github.com/ai-yash/st-chat)")
@@ -31,6 +26,27 @@ def query(payload):
 def get_text():
     input_text = st.text_input("You: ","Hello, how are you?", key="input")
     return input_text 
+
+
+user_input = get_text()
+
+if user_input:
+    output = query({
+        "inputs": {
+            "past_user_inputs": st.session_state.past,
+            "generated_responses": st.session_state.generated,
+            "text": user_input,
+        },"parameters": {"repetition_penalty": 1.33},
+    })
+
+    st.session_state.past.append(user_input)
+    st.session_state.generated.append(output["generated_text"])
+
+if st.session_state['generated']:
+
+    for i in range(len(st.session_state['generated'])-1, -1, -1):
+        message(st.session_state["generated"][i], key=str(i))
+        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
 
 
 user_input = get_text()
